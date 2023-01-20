23cd00a22f1475862b136a19cbd40e9087920f2a
unknown <nahuriragaston@gmail.com>
2023-01-20 11:46:15 +0300

    admin completed

--- src/Admin/Components/Header/Header.jsx
+++ src/Admin/Components/Header/Header.jsx
@@ -1,56 +1,64 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 // import "../Drawables/passport.jpeg";
 import "../Header/Header.css";
 import Logout from "../../../components/Login/Login";
 import Avatar from "..//..//../assets/profileimg.png";
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Menu, MenuItem } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import user from "..//../../app.config";
-
 
 const Header = () => {
   const [AnchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
   const handleOpenActions = (e) => {
-      setAnchorEl(e.currentTarget);
-    };
-    const handleCloseActions = () => {
-      setAnchorEl(null);
-    };
-  
-    const handleClickOpenDialog = () => {
-      setOpen(true);
-    };
-  
-    const handleCloseDialog = () => {
-      setOpen(false);
-    }
+    setAnchorEl(e.currentTarget);
+  };
+  const handleCloseActions = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClickOpenDialog = () => {
+    setOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
   return (
     <>
-    <header>
-      <h2>
-        <label htmlFor="nav-toggle">
-          <span className="las la-bars"></span>
-        </label>
-        <span className="health_unit_name">Hostel Management</span>
-      </h2>
-      <div className="" style={{ display: "flex", alignItems: "center" }}>
-        <div className="" style={{ fontSize: "42px", marginRight: 20 }}>
-          <i className="las la-bell"></i>
-        </div>
-        <div
-          className="user-wrapp"
-          aria-controls="reception-actions"
-          aria-haspopup="true"
-          onClick={handleOpenActions}
-          style={{ cursor: "pointer" }}
-        >
-          <img src={Avatar} alt="" width="40px" height="40px" />
-          <div className="">
-            <h4>{user ? `${user.name.split(" ")[0]}` : "Username"}</h4>
-            <small>{user.user.role}</small>
+      <header>
+        <h2>
+          <label htmlFor="nav-toggle">
+            <span className="las la-bars"></span>
+          </label>
+          <span className="health_unit_name">Hostel Management</span>
+        </h2>
+        <div className="" style={{ display: "flex", alignItems: "center" }}>
+          <div className="" style={{ fontSize: "42px", marginRight: 20 }}>
+            <i className="las la-bell"></i>
+          </div>
+          <div
+            className="user-wrapp"
+            aria-controls="reception-actions"
+            aria-haspopup="true"
+            onClick={handleOpenActions}
+            style={{ cursor: "pointer" }}
+          >
+            <img src={Avatar} alt="" width="40px" height="40px" />
+            <div className="">
+              {/* <h4>{user ? `${user.name.split(" ")[0]}` : "Username"}</h4> */}
+              <small>admin</small>
+            </div>
           </div>
         </div>
-      </div>
       </header>
 
       <Menu
@@ -63,13 +71,13 @@ const Header = () => {
       >
         <MenuItem onClick={handleCloseActions}>
           <span style={{ fontSize: 24, marginRight: 10 }}>
-         <i className="las la-user-alt"></i>
+            <i className="las la-user-alt"></i>
           </span>
           Profile
         </MenuItem>
         <MenuItem onClick={handleClickOpenDialog}>
           <span style={{ fontSize: 24, marginRight: 10 }}>
-         <i className="las la-sign-out-alt"></i>
+            <i className="las la-sign-out-alt"></i>
           </span>
           Log out
         </MenuItem>
@@ -109,7 +117,7 @@ const Header = () => {
           </Button>
         </DialogActions>
       </Dialog>
-      </>
+    </>
   );
 };
 export default Header;
--- src/HostelOwner/Routes/Home.jsx
+++ src/HostelOwner/Routes/Home.jsx
@@ -1,13 +1,32 @@
-import React, { useEffect,useState } from "react";
-import { Button, Menu, MenuItem, TextField } from "@mui/material"
-import { Link } from "react-router-dom"
-import Sidebar from "../Components/sidebar/Sidebar"
+import React, { useEffect, useState } from "react";
+import { Button, Menu, MenuItem, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import Sidebar from "../Components/sidebar/Sidebar";
 import Header from "../Components/Topbar/Header";
 import "./designs/home.css";
 import FormsApi from "../../api/api";
 import user from "../../app.config";
+import axios from "axios";
 
-const Home=()=>{
+const url = "http://localhost:5055/api/v6/allrooms";
+const Home = () => {
+  //displaying rooms that are not booked.
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setRooms(res.data.result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  const myrooms = rooms.filter((r) => {
+    return r.hostel_landlord === user.id;
+  });
+  console.log(myrooms);
+
   const [state, setState] = useState({
     booked_rooms: [],
     available_rooms: [],
@@ -17,273 +36,263 @@ const Home=()=>{
     dialog: false,
     message: "",
     mui: {
-        snackBarOpen: false,
-        snackBarMessage: "",
-        snackBarStatus: "info",
-        snackBarPosition: { vertical: "top", horizontal: "right" },
-      },
+      snackBarOpen: false,
+      snackBarMessage: "",
+      snackBarStatus: "info",
+      snackBarPosition: { vertical: "top", horizontal: "right" },
+    },
   });
   //mui popup
- const closePopUp=(event, reason)=>{
-    if(reason === "clickaway"){
-    return;
+  const closePopUp = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
     setState({
-        ...state,
-        mui:{
+      ...state,
+      mui: {
         ...state.mui,
         open: false,
         message: "Please Wait...",
         messageState: "info",
-        }
-      });
- }
- const handleClose = () => {
+      },
+    });
+  };
+  const handleClose = () => {
     setState({
-   ...state,
-    dialog: false
-   });
-    };
-    
-const handleCloseActionsRooms=()=>{
-    setState({ 
-   ...state,
-   AnchorElRooms: null,
-   });
-  }
-const handleOpenActions = (e) => {
-      setState({ ...state, 
-          AnchorEl: e.currentTarget });
-    };
-const handleCloseActions = () => {
-      setState({ ...state,
-           AnchorEl: null });
-    };
-const handleOpenActionsRooms = (e) => {
-        setState({ ...state, 
-        AnchorElRooms: e.currentTarget });
-      };
+      ...state,
+      dialog: false,
+    });
+  };
 
-      useEffect(() => {
-        (async () => {
-          const res = await new FormsApi().get("/rooms/" + user.id);
-          if (res === "Error") {
-            console.log(res);
-          } else {
-            if (res.status) {
-              let available_rooms = [];
-              let booked_rooms = [];
-              res.result.forEach((el) => {
-                if (el.booked) {
-                  booked_rooms = [...booked_rooms, el];
-                } else {
-                  available_rooms = [...available_rooms, el];
-                }
-              });
-              setState({ ...state, available_rooms, booked_rooms });
+  const handleCloseActionsRooms = () => {
+    setState({
+      ...state,
+      AnchorElRooms: null,
+    });
+  };
+  const handleOpenActions = (e) => {
+    setState({ ...state, AnchorEl: e.currentTarget });
+  };
+  const handleCloseActions = () => {
+    setState({ ...state, AnchorEl: null });
+  };
+  const handleOpenActionsRooms = (e) => {
+    setState({ ...state, AnchorElRooms: e.currentTarget });
+  };
+
+  useEffect(() => {
+    (async () => {
+      const res = await new FormsApi().get("/rooms/" + user.id);
+      if (res === "Error") {
+        // console.log(res);
+      } else {
+        if (res.status) {
+          let available_rooms = [];
+          let booked_rooms = [];
+          res.result.forEach((el) => {
+            if (el.booked) {
+              booked_rooms = [...booked_rooms, el];
+            } else {
+              available_rooms = [...available_rooms, el];
             }
-          }
-        })();
-      }, []);
+          });
+          setState({ ...state, available_rooms, booked_rooms });
+        }
+      }
+    })();
+  }, []);
 
-    return(
-        <>
-        <input type="checkbox" id="nav-toggle" defaultChecked />
-        <Sidebar active = "home" />
-            <div className="main_ctr">
-           <Header /> 
-           <main>
-            <div className="cards_ctr">
+  return (
+    <>
+      <input type="checkbox" id="nav-toggle" defaultChecked />
+      <Sidebar active="home" />
+      <div className="main_ctr">
+        <Header />
+        <main>
+          <div className="cards_ctr">
             <div className="single_card">
-                <div className="">
-                  <h3>10</h3>
-                  <span>
-                    Total Bookings <br />
-                  </span>
-                </div>
-                <div className="">
-                  <span className="las la-dolly-flatbed"> </span>
-                </div>
+              <div className="">
+                <h3>10</h3>
+                <span>
+                  Total Bookings <br />
+                </span>
               </div>
-              <div className="single_card">
-                <div className="">
-                  <h3>{`${state.available_rooms.length}`}</h3>
-                  <span>Free Rooms</span>
-                </div>
-                <div className="">
-                  <span className="las la-dolly-flatbed"></span>
-                </div>
-                </div>
-                <div className="single_card">
-                <div className="">
-                  <h3>{`${state.booked_rooms.length}`}</h3>
-                  <span>Occupied Rooms</span>
-                </div>
-                <div className="">
-                  <span className="las la-dolly-flatbed"> </span>
-                </div>
+              <div className="">
+                <span className="las la-dolly-flatbed"> </span>
               </div>
-              <div className="single_card">
-                <div className="">
-                  <h3>30%</h3>
-                  <span>Performance</span>
+            </div>
+            <div className="single_card">
+              <div className="">
+                <h3>{`${state.available_rooms.length}`}</h3>
+                <span>Free Rooms</span>
+              </div>
+              <div className="">
+                <span className="las la-dolly-flatbed"></span>
+              </div>
+            </div>
+            <div className="single_card">
+              <div className="">
+                <h3>{`${state.booked_rooms.length}`}</h3>
+                <span>Occupied Rooms</span>
+              </div>
+              <div className="">
+                <span className="las la-dolly-flatbed"> </span>
+              </div>
+            </div>
+            <div className="single_card">
+              <div className="">
+                <h3>30%</h3>
+                <span>Performance</span>
+              </div>
+              <div className="">
+                <span className="las la-user-tag"> </span>
+              </div>
+            </div>
+          </div>
+          <div className="top_btns">
+            <div>
+              <h2>My Hostels</h2>
+            </div>
+            <div>
+              <Link to="/hostels/new">
+                <Button variant="contained" color="primary">
+                  New Hostel
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <div className="rooms_grid">
+            <div className="recent_project_ctr">
+              <div className="card">
+                <div className="card-header">
+                  <h3>Available Rooms</h3>
+                  <TextField
+                    name="name"
+                    variant="outlined"
+                    label="Search Room"
+                    style={{
+                      width: "20%",
+                    }}
+                  />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    aria-controls="room-actions"
+                    aria-haspopup="true"
+                    onClick={handleOpenActionsRooms}
+                  >
+                    Menu
+                    <span style={{ fontSize: "17.5px", marginLeft: "10px" }}>
+                      <span className="las la-angle-down"></span>
+                    </span>
+                  </Button>
+                  <Menu
+                    id="room-actions"
+                    anchorEl={state.AnchorElRooms}
+                    keepMounted
+                    open={Boolean(state.AnchorElRooms)}
+                    onClose={handleCloseActionsRooms}
+                    disableScrollLock={true}
+                  >
+                    <Link to="/new-room">
+                      <MenuItem onClick="">New Room</MenuItem>
+                    </Link>
+                  </Menu>
                 </div>
-                <div className="">
-                  <span className="las la-user-tag"> </span>
+                <div className="card-body">
+                  <table width="100%">
+                    <thead>
+                      <tr>
+                        <td>Number</td>
+                        <td>Room Fee(UGX)</td>
+                        <td>Room Type</td>
+                        <td>Edit</td>
+                        <td>Status</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {state.available_rooms.length === 0 ? (
+                        <tr>
+                          <td>No rooms to display...</td>
+                        </tr>
+                      ) : (
+                        state.available_rooms.map((v, i) => {
+                          return (
+                            <tr key={i}>
+                              <td>{v.room_number}</td>
+                              <td>{v.room_fee}</td>
+                              <td>{v.room_type}</td>
+                              <td>
+                                <Button
+                                  onClick={(e) => {
+                                    setState({
+                                      ...state,
+                                    });
+                                  }}
+                                >
+                                  Edit
+                                </Button>
+                              </td>
+                              <Button
+                                onClick={(e) => {
+                                  setState({
+                                    ...state,
+                                  });
+                                }}
+                              >
+                                Book
+                              </Button>
+                            </tr>
+                          );
+                        })
+                      )}
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
-            <div className="top_btns">
-                <div>
-                  <h2>My Hostels</h2>
+            <div className="recent_project_ctr">
+              <div className="card">
+                <div className="card-header">
+                  <h3>Booked Rooms</h3>
                 </div>
-                <div>
-                  <Link to="/hostels/new">
-                    <Button variant="contained" color="primary">
-                    New Hostel
-                    </Button>
-                  </Link>
-                </div>
-              </div>
-              <div className="rooms_grid">
-                <div className="recent_project_ctr">
-                    <div className="card">
-                        <div className="card-header">
-                          <h3>Available Rooms</h3>
-                              <TextField
-                              name="name"
-                              variant="outlined"
-                              label="Search Room"
-                              style={{
-                              width: "20%",
-                              }}
-                    />
-                      <Button
-                      variant="contained"
-                      color="primary"
-                      aria-controls="room-actions"
-                      aria-haspopup="true"
-                      onClick={handleOpenActionsRooms}
-                    >
-                      Menu
-                      <span style={{ fontSize: "17.5px", marginLeft: "10px" }}>
-                      <span className="las la-angle-down"></span>
-                      </span>
-                    </Button>
-                    <Menu
-                      id="room-actions"
-                      anchorEl={state.AnchorElRooms}
-                      keepMounted
-                      open={Boolean(state.AnchorElRooms)}
-                      onClose={handleCloseActionsRooms}
-                      disableScrollLock={true}
-                    >
-                      <Link to="/new-room">
-                        <MenuItem onClick="">
-                          New Room
-                        </MenuItem>
-                      </Link>
-                    </Menu>
-                        </div>
-                        <div className="card-body">
-                          <table  width="100%">
-                            <thead>
-                                <tr>
-                                <td>Number</td>
-                                <td>Room Fee(UGX)</td>
-                                <td>Room Type</td>
-                                <td>Edit</td>
-                                <td>Status</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {state.available_rooms.length === 0 ? (
-                              <tr>
-                                <td>No rooms to display...</td>
-                              </tr>
-                            ) : (
-                              state.available_rooms.map((v, i)=>{
-                                return(
-                                  <tr key={i}>
-                                    <td>{v.room_number}</td>
-                                    <td>{v.room_fee}</td>
-                                    <td>{v.room_type}</td>
-                                    <td>
-                                    <Button
-                                      onClick={(e) => {
-                                        setState({
-                                          ...state,
-                                          
-                                        });
-                                      }}
-                                    >
-                                      Edit
-                                    </Button>
-                                  </td>
-                                  <Button
-                                      onClick={(e) => {
-                                        setState({
-                                          ...state,
-                                          
-                                        });
-                                      }}
-                                    >
-                                      Book
-                                    </Button>
-                                  </tr>
-                                )
-                              })
-                            )}
-                            </tbody>
-                          </table>
-                        </div>
-                    </div>
-                </div>
-                <div className="recent_project_ctr">
-                  <div className="card">
-                    <div className="card-header">
-                      <h3>Booked Rooms</h3>
-                    </div>
-                    <div className="card-body">
-                    <table width="100%">
-                      <thead>
-                        <tr>
-                          <td>Number</td>
-                          <td>Room Fee(UGX)</td>
-                          <td>Room type</td>
-                          <td>
-                              <Button>
-                              Remove
-                              </Button>
-                            </td>
-                        </tr>
-                      </thead>
-                      <tbody>
+                <div className="card-body">
+                  <table width="100%">
+                    <thead>
+                      <tr>
+                        <td>Number</td>
+                        <td>Room Fee(UGX)</td>
+                        <td>Room type</td>
+                        <td>
+                          <Button>Remove</Button>
+                        </td>
+                      </tr>
+                    </thead>
+                    <tbody>
                       {state.booked_rooms.length === 0 ? (
                         <tr>
                           <td>No rooms to display...</td>
                         </tr>
                       ) : (
-                        state.booked_rooms.map((v, i)=>{
-                          return(
+                        state.booked_rooms.map((v, i) => {
+                          return (
                             <tr key={i}>
                               <td>{v.room_number}</td>
                               <td>{v.room_fee}</td>
                               <td>{v.room_type}</td>
                             </tr>
-                          )
+                          );
                         })
-                      )
-                        }
-                      </tbody>
-                      </table>  
-                    </div>
-                  </div>
+                      )}
+                    </tbody>
+                  </table>
                 </div>
               </div>
-            </main>          
             </div>
-        </>
-    )
-}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
 export default Home;

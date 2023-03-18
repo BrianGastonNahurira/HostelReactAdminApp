2671accb70bce448de38e0c28148e775695b3644
unknown <nahuriragaston@gmail.com>
2023-03-18 17:44:19 +0300

    re

--- src/Admin/Components/Tables/Table.jsx
+++ src/Admin/Components/Tables/Table.jsx
@@ -17,6 +17,9 @@ export const StateTable = () => {
   const [state, setState] = useState({
     allbookings: [],
   });
+  const [request, setRequest] = useState({
+    landlordRequests: [],
+  });
 
   useEffect(() => {
     (async () => {
@@ -32,69 +35,146 @@ export const StateTable = () => {
           setState({ ...state, allbookings });
         }
       }
+
+      const response = await new FormsApi().get("/alllandlordrequests");
+      if (response === "Error") {
+        console.log(response);
+      } else {
+        if (response.status) {
+          let landlordRequests = [];
+          response.result.forEach((l) => {
+            landlordRequests.push(l);
+          });
+          setRequest({ ...request, landlordRequests });
+        }
+      }
     })();
   }, []);
 
   return (
-    <TableContainer component={Paper} sx={{ maxHeight: "437px" }}>
-      <Table aria-aria-label="simple table" stickyHeader>
-        <TableHead>
-          <TableRow></TableRow>
-          <TableRow>
-            <TableCell
-              style={{
-                color: "blue",
-                fontWeight: "bolder",
-                fontSize: "1rem",
-              }}
-            >
-              Name
-            </TableCell>
-            <TableCell
-              style={{
-                color: "blue",
-                fontWeight: "bolder",
-                fontSize: "1rem",
-              }}
-            >
-              Hostel
-            </TableCell>
-            <TableCell
-              style={{
-                color: "blue",
-                fontWeight: "bolder",
-                textAlign: "center",
-                fontSize: "1rem",
-              }}
-            >
-              Room Number
-            </TableCell>
-            <TableCell
-              style={{
-                color: "blue",
-                fontWeight: "bolder",
-                fontSize: "1rem",
-              }}
-              align="center"
-            >
-              Level
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {state.allbookings.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ "&:last-child td, &last-child th": { border: 0 } }}
-            >
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.name_of_hostel}</TableCell>
-              <TableCell align="center">{row.room_number}</TableCell>
-              <TableCell align="center">{row.level}</TableCell>
+    <>
+      <TableContainer component={Paper} sx={{ maxHeight: "437px" }}>
+        <Table aria-aria-label="simple table" stickyHeader>
+          <TableHead>
+            <TableRow></TableRow>
+            <TableRow>
+              <TableCell
+                style={{
+                  color: "blue",
+                  fontWeight: "bolder",
+                  fontSize: "1rem",
+                }}
+              >
+                Name
+              </TableCell>
+              <TableCell
+                style={{
+                  color: "blue",
+                  fontWeight: "bolder",
+                  fontSize: "1rem",
+                }}
+              >
+                Hostel
+              </TableCell>
+              <TableCell
+                style={{
+                  color: "blue",
+                  fontWeight: "bolder",
+                  textAlign: "center",
+                  fontSize: "1rem",
+                }}
+              >
+                Room Number
+              </TableCell>
+              <TableCell
+                style={{
+                  color: "blue",
+                  fontWeight: "bolder",
+                  fontSize: "1rem",
+                }}
+                align="center"
+              >
+                Level
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {state.allbookings.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &last-child th": { border: 0 } }}
+              >
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.name_of_hostel}</TableCell>
+                <TableCell align="center">{row.room_number}</TableCell>
+                <TableCell align="center">{row.level}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <div>
+        <h2 style={{ textAlign: "center", color: "gray", margin: "10px 0px" }}>
+          Landlord requests
+        </h2>
+
+        <TableContainer component={Paper} sx={{ maxHeight: "437px" }}>
+          <Table aria-aria-label="simple table" stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell
+                  style={{
+                    color: "blue",
+                    fontWeight: "bolder",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Name
+                </TableCell>
+                <TableCell
+                  style={{
+                    color: "blue",
+                    fontWeight: "bolder",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Email
+                </TableCell>
+                <TableCell
+                  style={{
+                    color: "blue",
+                    fontWeight: "bolder",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Number
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            {request.landlordRequests.length === 0 ? (
+              <div>
+                <p>No landlord requests to display........</p>
+              </div>
+            ) : (
+              request.landlordRequests.map((v, i) => {
+                return (
+                  <TableBody>
+                    <TableRow
+                      key={i}
+                      sx={{ "&:last-child td, &last-child th": { border: 0 } }}
+                    >
+                      <TableCell>{v.name}</TableCell>
+                      <TableCell>{v.email}</TableCell>
+                      <TableCell align="center">{v.phone}</TableCell>
+                    </TableRow>
+                  </TableBody>
+                );
+              })
+            )}
+          </Table>
+        </TableContainer>
+      </div>
+    </>
   );
 };
